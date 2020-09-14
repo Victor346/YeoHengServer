007f73e269374fe718aacecad005632dba378f33
Victor Lopez <A01336434@itesm.mx>
2020-09-13 19:11:54 -0500

    Added initial JWT implementation

--- src/auth/authentication.rs
+++ src/auth/authentication.rs
@@ -1,4 +1,17 @@
+use crate::models::user::User;
+
+use bson::oid::ObjectId;
+use serde::{Serialize, Deserialize};
 use argon2::{self, Config};
+use jsonwebtoken::{encode, decode, Header, Algorithm, Validation,
+    EncodingKey, DecodingKey};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MyClaims {
+    iss: String,
+    sub: String,
+    exp: usize,
+}
 
 pub fn salt_password(password: String) -> String {
     let pass = password.as_bytes();
@@ -8,4 +21,13 @@ pub fn salt_password(password: String) -> String {
 
     let config  = Config::default();
     argon2::hash_encoded(pass, salt, &config).unwrap()
+}
+
+pub fn generate_jwt(user: User) -> String{
+    let claims = MyClaims {
+        iss: "yeoheng-server.com".to_string(),
+        sub: user._id.unwrap().to_hex(),
+        exp: 4552,
+    };
+    "agfsf".to_string()
 }
